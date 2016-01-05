f3d86df89a3cbb5b8b07eb640ed4436b50d4e86a
cirthix <cirthix@gmail.com>
2016-01-06 07:01:08 +0800

    Update OverclockExample.ino

--- Overclock/examples/OverclockExample/OverclockExample.ino
+++ Overclock/examples/OverclockExample/OverclockExample.ino
@@ -24,19 +24,18 @@
  This is the example project for demonstrating operation of the Overclock library.
 Made by cirthix (cirthix@gmail.com)
 Please see https://github.com/cirthix/Overclock for more information.
-
-
+Note the scaled delay as used here:   delay(CLOCK_RATIO*100);
  Expected output should be roughly this (using atmega168p at 3.3v at 20C:
 Hello from setup().
+OC Clock: 15094328
 Default Clock: 8000000
-OC Clock: 15625272
-16M Clock: 15639912
-14M Clock: 14091976
-12M Clock: 12006264
-10M Clock: 9984968
+16M Clock: 15644304
+14M Clock: 14057328
+12M Clock: 11965760
+10M Clock: 10057192
 8M Clock: 8000000
-6M Clock: 5960432
-4M Clock: 5111312
+6M Clock: 6000936
+4M Clock: 5096184
 */
 
 
@@ -45,10 +44,23 @@ OC Clock: 15625272
 #include <avr/wdt.h>
 #include "Overclock.h"
 
-const uint32_t SERIAL_BAUD = 115200 ;  // Recommend 38400, 19200, or 9600 for better stability.  115200 works for me, but may be less stable in some setups.
+#define OVERCLOCKING
 
-Overclock my_Overclock_object=Overclock((uint32_t)16000000, (uint32_t)SERIAL_BAUD);
-//Overclock my_Overclock_object=Overclock((uint32_t)F_CPU);
+
+const uint32_t SERIAL_BAUD = 115200 ;  // Recommend 38400, 19200, or 9600 for better stability.  115200 works for me, but may be less stable in some setups.
+const uint32_t OVERCLOCKED_SPEED = 15000000; // Note: setting this outside of the OCCAL tuning range will break serial communications.
+
+#ifdef OVERCLOCKING
+const uint32_t REAL_SPEED=OVERCLOCKED_SPEED;
+#else
+const uint32_t REAL_SPEED=F_CPU;
+#endif
+const float CLOCK_RATIO=REAL_SPEED/F_CPU;
+
+
+
+Overclock my_Overclock_object=Overclock(REAL_SPEED, SERIAL_BAUD);
+//Overclock my_Overclock_object=Overclock(REAL_SPEED);
 //Overclock my_Overclock_object=Overclock();
 
 ISR(WDT_vect) {
@@ -64,32 +76,32 @@ void setup() {
 }
 
 void loop() {
+  my_Overclock_object.SetClockSpeedOC();
+  Serial.print(F("OC Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
+  delay(CLOCK_RATIO*100);
   my_Overclock_object.SetClockSpeedStock();        
   Serial.print(F("Default Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);  
-  my_Overclock_object.SetClockSpeedOCMax();
-  Serial.print(F("OC Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)16000000);
+  delay(CLOCK_RATIO*100);  
+  my_Overclock_object.SetClockSpeed(16000000);
   Serial.print(F("16M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)14000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(14000000);
   Serial.print(F("14M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)12000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(12000000);
   Serial.print(F("12M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)10000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(10000000);
   Serial.print(F("10M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)8000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(8000000);
   Serial.print(F("8M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)6000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(6000000);
   Serial.print(F("6M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
-  my_Overclock_object.SetClockSpeed((uint32_t)4000000);
+  delay(CLOCK_RATIO*100);
+  my_Overclock_object.SetClockSpeed(4000000);
   Serial.print(F("4M Clock: ")); Serial.println(my_Overclock_object.GetClockSpeed());  Serial.flush();
-  delay(100);
+  delay(CLOCK_RATIO*100);
   while(1){;}
 }
